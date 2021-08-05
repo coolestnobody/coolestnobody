718647202848543626b5470986f63b29dd6469a1
sophia <carlsonsophial@gmail.com>
2021-08-04 19:02:02 -0700

    restyled home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,18 +9,50 @@ const Home = (props) => {
   return (
     <React.Fragment>
       <div className="home-container">
-        <img src={logo} className="home-img" alt="logo" />
-        {/* <img src="../../resources/pic.jpg" alt="" className="home-img"/> */}
-        <div className="home-text-container">
-          <p className="home-text">coolestnobody.com coming soon 😳</p>
-          <p className="home-text">lol</p>
-          <a href="https://www.instagram.com/coolestnobodyart/" target="_blank">
-            <i className="fa fa-instagram fa-lg"></i>
-          </a>
-          <a href="https://twitter.com/3rdlittlebird" target="_blank">
-            <i className="fa fa-twitter fa-lg"></i>
-          </a>
-        </div>
+
+        <nav className="navbar">
+          <div className="socials">
+            <a href="https://www.instagram.com/coolestnobodyart/" target="_blank">
+              <i className="fa fa-instagram fa-lg"></i>
+            </a>
+            <a href="https://twitter.com/3rdlittlebird" target="_blank">
+              <i className="fa fa-twitter fa-lg"></i>
+            </a>
+          </div>
+
+          <ul className="nav-list">
+            <li>
+              <a id="welcome-section">About</a>
+            </li>
+            <li>
+              <a id="projects">Work</a>
+            </li>
+            <li>
+              <a id="contact">Contact</a>
+              </li>
+          </ul>
+        </nav>
+
+
+        <section id="welcome-section" className="welcome-section">
+          {/* <img src={logo} className="home-img" alt="logo" /> */}
+          <h1>i am coolestnobody.</h1>
+          <p>cogito, ergo sum.</p>
+        </section>
+
+        <section id="projects" className="projects-section">
+          <h2>These are some of my projects</h2>
+        </section>
+
+        <section id="contact" className="contact-section">
+          <div>
+            <h2>Let's work together...</h2>
+            <p>Contact me!</p>
+          </div>
+        </section>
+
+
+
       </div>
 
       {/* <Footer /> */}
